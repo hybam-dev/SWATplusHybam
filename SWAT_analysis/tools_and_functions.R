d5f1fee8e91335788f7ba6b994094a5fcb0fbd2a
florentpapini <33873483+florentpapini@users.noreply.github.com>
2021-05-10 10:19:39 +0200

    deleting demo file

--- SWAT_analysis/tools_and_functions.R
+++ SWAT_analysis/tools_and_functions.R
@@ -7,16 +7,6 @@
 ## (florent.papini@ird.fr)
 ##################################################
 
-library(tidyr)
-library(dplyr)
-library(ggplot2)
-library(dygraphs)
-library(xts)
-library(readr)
-library(hydroGOF)
-library(plotly)
-library(lubridate)
-
 
 ## View timeseries:
 ##################################################
@@ -186,7 +176,7 @@ setup_input_files <- function(path, files)
         hyd = paste("hydro", file, sep="    ")
       }
       else {
-      hyd = paste(hyd, file, sep="    ")
+        hyd = paste(hyd, file, sep="    ")
       }
     }
     
@@ -195,7 +185,7 @@ setup_input_files <- function(path, files)
         wl = paste("wl", file, sep="    ")
       }
       else {
-      wl = paste(wl, file, sep="    ")
+        wl = paste(wl, file, sep="    ")
       }
     }
     
@@ -204,7 +194,7 @@ setup_input_files <- function(path, files)
         sands = paste("sands", file, sep="    ")
       }
       else {
-      sands = paste(sands, file, sep="    ")
+        sands = paste(sands, file, sep="    ")
       }
     }
     
@@ -213,7 +203,7 @@ setup_input_files <- function(path, files)
         da = paste("da", file, sep="    ")
       }
       else {
-      da = paste(da, file, sep="    ")
+        da = paste(da, file, sep="    ")
       }
     }
     
@@ -237,13 +227,19 @@ setup_par_bounds <- function(path)
   # clean the old file input infos
   my_new_file = readLines(paste(path,"/cal_parms.cal", sep=""))
   my_new_file = my_new_file[-(187:191)]
+  my_new_file[2] = "195"
   write(my_new_file, file=paste(path,"/cal_parms.cal", sep=""))
   
   # create new lines
   bounds = c("no_rte                         bsn       0.00000      10.00000              null
 fpgeom                         bsn       0.00000      10.00000              null
 theta_fp                       bsn       0.00000      10.00000              null
-alpha_f                        bsn       0.00000      10.00000              null")
+alpha_f                        bsn       0.00000      10.00000              null
+ci                             bsn       0.00000      10.00000              null
+mkkco1                         bsn       0.00000      10.00000              null
+mkkco2                         bsn       0.00000      10.00000              null
+mkkco3                         bsn       0.00000      10.00000              null
+mkkx                           bsn       0.00000      10.00000              null")
   
   write(bounds, file=paste(path,"/cal_parms.cal", sep=""), append=TRUE)
   
@@ -251,6 +247,32 @@ alpha_f                        bsn       0.00000      10.00000              null
 }
 
 
+## Write input files:
+##################################################
+setup_new_ch_parm <- function(path) 
+{
+  my_file<-file(paste(path,"/hyd-sed-lte.cha", sep=""))
+  
+  my_new_file = readLines(paste(path,"/hyd-sed-lte.cha", sep=""))
+  
+  # New header
+  my_new_file[2] = paste(substr(my_new_file[2], 1, 328), "           kfp   description", sep="")
+  
+  n = 3
+  # Adding new parameters
+  for (line in my_new_file) {
+    if(line == my_new_file[1] | line == my_new_file[2])
+      next
+    line = paste(substr(line, 1, 328), "       5.00000", sep="")
+    # Replacing with the new line
+    my_new_file[n] = line
+    n = n + 1
+  }
+  
+  writeLines(my_new_file, my_file)
+  
+  close(my_file)
+}
 
 
 
